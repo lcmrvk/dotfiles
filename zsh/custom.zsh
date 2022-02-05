6cbde8f39676acc404e40301fc87cc66b2b72da3
lcmrvk <lcmrvk@arch.home>
2022-02-05 21:46:12 +0100

    Add zsh related configs

---
+++ zsh/custom.zsh
@@ -0,0 +1,40 @@
+# list directory shortcuts
+alias l='ls -CF'
+alias ll='ls -alF'
+alias la='ls -A'
+
+# go back a directory shortcut
+cd_multiple() {
+    args=$@
+    if [ $args = "" ]; then
+        dir="."
+    else
+        dir=$(echo "$args" | sed 's/,/..\//g')
+    fi
+    cd "$dir"
+}
+
+alias .,='cd_multiple'
+alias ..='cd ..'
+alias .s='cd .. ; ls'
+
+# python
+alias py3='python3'
+
+# git related
+alias gs='git status'
+alias gl='git log'
+alias gd='git diff'
+alias gb='git branch'
+
+# ranger shortcut
+ranger_cd() {
+    temp_file="$(mktemp -t "ranger_cd.XXXXXXXXXX")"
+    ranger --choosedir="$temp_file" -- "${@:-$PWD}"
+    if chosen_dir="$(cat -- "$temp_file")" && [ -n "$chosen_dir" ] && [ "$chosen_dir" != "$PWD" ]; then
+        cd -- "$chosen_dir"
+    fi
+    rm -f -- "$temp_file"
+}
+
+alias rr='ranger_cd'
